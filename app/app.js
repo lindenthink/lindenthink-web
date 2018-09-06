e9887cb2a2f0e9831f73c586a04f426a23e1a323
qinlinchogn@163.com <a123456>
2018-09-07 00:37:45 +0800

    菜单固定在顶部显示

--- app/app.js
+++ app/app.js
@@ -8,3 +8,14 @@ import './public/img/app-enjoy.jpg'
 import './public/img/app-fund.jpg'
 
 import './app.css'
+
+$('body').scroll(function () {
+    console.info('scrolling...');
+    if (document.body.scrollTop >= 100 && !$('nav').hasClass('fixed-top')) {
+        $('nav').addClass('fixed-top');
+    } else {
+        if ($('nav').hasClass('fixed-top')) {
+            $('nav').removeClass('fixed-top');
+        }
+    }
+});
