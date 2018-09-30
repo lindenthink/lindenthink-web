04409de6bedce8be9ef16d04ab137eadb0cb1f02
qinlinchong <qinlinchong@hddomain.cn>
2018-09-30 17:41:26 +0800

    调整菜单展示效果

--- app/wealth/wealth.js
+++ app/wealth/wealth.js
@@ -5,9 +5,11 @@ import 'bootstrap-treeview/dist/bootstrap-treeview.min'
 const tree = [
     {
         text: '编程',
+        selectable: false,
         nodes: [
             {
                 text: 'JAVA',
+                selectable: false,
                 nodes: [
                     {
                         text: '基础'
@@ -23,21 +25,58 @@ const tree = [
         ]
     },
     {
-        text: '操作系统'
+        text: '操作系统',
+        selectable: false,
+        nodes: [
+            {
+                text: 'Windows'
+            },
+            {
+                text: 'Linux'
+            }
+        ]
     },
     {
-        text: '数据库'
+        text: '数据库',
+        selectable: false,
+        nodes: [
+            {
+                text: 'Mysql'
+            },
+            {
+                text: 'Oracle'
+            },
+            {
+                text: 'MongoDB'
+            }
+        ]
     },
     {
-        text: '算法'
+        text: '算法',
+        selectable: false,
+        nodes: [
+            {
+                text: '排序'
+            }
+        ]
     },
     {
-        text: '数学'
+        text: '数学',
     }
 ];
 
 $('#tree').treeview({
     data: tree,
-    levels: 5,
-    checkedIcon: 'glyphicon glyphicon-stop'
+    levels: 10,
+    collapseIcon:'glyphicon glyphicon-chevron-down',
+    expandIcon:'glyphicon glyphicon-chevron-right',
+    emptyIcon:'glyphicon glyphicon-folder-close',
+    showTags: true,
+    onNodeSelected: (event, node) => {
+        console.info('选择[' + node.text+']菜单');
+        // TODO
+    }
 });
+
+$('#tree').treeview('collapseAll', { silent: true }); // 默认关闭菜单
+
