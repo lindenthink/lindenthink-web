7fa14aa1e8e6ea1731afe0d4acb1cc21a03c8bf9
qinlinchong <qinlinchong@hd123.com>
2025-03-15 21:23:46 +0800

    优化工具样式

--- src/views/Tools.vue
+++ src/views/Tools.vue
@@ -4,77 +4,34 @@
     <a-layout-content>
       <div class="tools">
         <div class="tools-menu">
-
-          <a-dropdown>
-            <a class="ant-dropdown-link" @click.prevent>
-              格式化
-              <DownOutlined />
-            </a>
-            <template #overlay>
-              <a-menu>
-                <a-menu-item key="1">
-                  <router-link to="/tools/format-json">JSON</router-link>
-                </a-menu-item>
-                <a-menu-item key="2">
-                  <router-link to="/tools/format-xml">XML</router-link>
-                </a-menu-item>
-                <a-menu-item key="3">
-                  <router-link to="/tools/format-sql">SQL</router-link>
-                </a-menu-item>
-              </a-menu>
-            </template>
-          </a-dropdown>
-          <a-divider type="vertical" />
-          <a-dropdown>
-            <a class="ant-dropdown-link" @click.prevent>
-              编解码
-              <DownOutlined />
-            </a>
-            <template #overlay>
-              <a-menu>
-                <a-menu-item key="1">Base64</a-menu-item>
-                <a-menu-item key="2">Unicode</a-menu-item>
-                <a-menu-item key="3">URL</a-menu-item>
-                <a-menu-item key="4">Ascii</a-menu-item>
-              </a-menu>
-            </template>
-          </a-dropdown>
-          <a-divider type="vertical" />
-          <a-dropdown>
-            <a class="ant-dropdown-link" @click.prevent>
-              加解密
-              <DownOutlined />
-            </a>
-            <template #overlay>
-              <a-menu @click="onClick">
-                <a-menu-item key="1">AES</a-menu-item>
-                <a-menu-item key="2">DES</a-menu-item>
-                <a-menu-item key="3">RSA</a-menu-item>
-                <a-menu-item key="4">SM</a-menu-item>
-              </a-menu>
-            </template>
-          </a-dropdown>
-          <a-divider type="vertical" />
-          <a-dropdown>
-            <a class="ant-dropdown-link" @click.prevent>
-              转换
-              <DownOutlined />
-            </a>
-            <template #overlay>
-              <a-menu @click="onClick">
-                <a-menu-item key="1">时间戳</a-menu-item>
-                <a-menu-item key="2">二维码</a-menu-item>
-                <a-menu-item key="3">Cron表达式</a-menu-item>
-              </a-menu>
-            </template>
-          </a-dropdown>
+          <template v-for="(tool, index) in tools" :key="tool.title">
+            <a-dropdown>
+              <a class="ant-dropdown-link" @click.prevent>
+                {{ tool.title }}
+                <DownOutlined />
+              </a>
+              <template #overlay>
+                <a-menu>
+                  <a-menu-item v-for="child in tool.children" :key="child.title">
+                    <router-link :to="child.path">{{ child.title }}</router-link>
+                  </a-menu-item>
+                </a-menu>
+              </template>
+            </a-dropdown>
+            <a-divider v-if="index !== tools.length - 1" type="vertical" />
+          </template>
+          <a-divider dashed plain><span style="color: lightgrey;">快捷入口</span></a-divider>
+          <div class="tools-quick-entry">
+            <router-link v-for="child in quickEntries" :key="child.title" :to="child.path" class="quick-entry-link">
+              {{ child.quickName }}
+            </router-link>
+          </div>
         </div>
 
         <div class="tools-body">
           <router-view> </router-view>
         </div>
       </div>
-
     </a-layout-content>
     <a-layout-sider></a-layout-sider>
   </a-layout>
@@ -85,22 +42,92 @@ import { ref, onMounted } from 'vue'
 import { useRouter } from 'vue-router'
 import { DownOutlined } from '@ant-design/icons-vue'
 
-
 const router = useRouter()
 onMounted(() => {
   router.push({ path: '/tools' })
 })
 
+const tools = [
+  {
+    title: '格式化',
+    children: [
+      { title: 'JSON', path: '/tools/format-json', quickEntry: true, quickName: 'JSON格式化' },
+      { title: 'XML', path: '/tools/format-xml' },
+      { title: 'SQL', path: '/tools/format-sql' },
+    ],
+  },
+  {
+    title: '编解码',
+    children: [
+      { title: 'Base64', path: '/tools/encode-base64', quickEntry: true, quickName: 'Base64编解码' },
+      { title: 'Unicode', path: '/tools/encode-unicode' },
+      { title: 'URL', path: '/tools/encode-url' },
+      { title: 'Ascii', path: '/tools/encode-ascii' },
+    ],
+  },
+  {
+    title: '加解密',
+    children: [
+      { title: 'AES', path: '/tools/encrypt-aes' },
+      { title: 'DES', path: '/tools/encrypt-des' },
+      { title: 'RSA', path: '/tools/encrypt-rsa', quickEntry: true, quickName: `RSA加解密` },
+      { title: 'SM', path: '/tools/encrypt-sm' },
+    ],
+  },
+  {
+    title: '转换',
+    children: [
+      { title: '时间戳', path: '/tools/convert-timestamp', quickEntry: true, quickName: '时间戳转换' },
+      { title: 'Cron表达式', path: '/tools/convert-cron', quickEntry: true, quickName: 'Cron表达式转换' },
+      { title: '进制', path: '/tools/convert-radix' },
+    ],
+  },
+  {
+    title: '图片',
+    children: [
+      { title: 'BASE64', path: '/tools/image-base64' },
+      { title: '二维码', path: '/tools/image-qrcode', quickEntry: true, quickName: '二维码生成' },
+    ],
+  },
+]
+const quickEntries = tools.flatMap(tool => tool.children.filter(child => child.quickEntry))
 </script>
 
 <style scoped lang="less">
 .tools {
   padding: 10px;
   margin: 10px;
+
   .tools-menu {
     border: 1px solid #f0f2f5;
-    padding: 10px ;
+    padding: 15px;
     margin-bottom: 10px;
+    border-radius: 8px;
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
   }
+
+  .tools-quick-entry {
+    display: flex;
+    gap: 10px;
+    flex-wrap: wrap;
+
+    .quick-entry-link {
+      padding: 0px 5px;
+      border: 1px solid #afaeae;
+      border-radius: 4px;
+      text-decoration: none;
+      color: #afaeae;
+      transition: background-color 0.3s;
+
+      &:hover {
+        color: #1890ff;
+        border-color: #1890ff;
+      }
+    }
+  }
+}
+
+.ant-divider-horizontal.ant-divider-with-text {
+  margin: 5px 0;
 }
 </style>
