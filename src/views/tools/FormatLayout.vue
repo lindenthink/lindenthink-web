9487c31e5387cfde650ee75093d39f7d3e0a94e5
qinlinchong <qinlinchong@hd123.com>
2025-07-09 20:33:00 +0800

    调整工具页面样式

--- src/views/tools/FormatLayout.vue
+++ src/views/tools/FormatLayout.vue
@@ -72,8 +72,7 @@ const onCopy = () => {
 <style scoped lang="less">
 .form-card {
   height: auto;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
-  border-radius: 8px;
+  border-radius: 2px;
 
   :deep(.ant-card-head) {
     border-bottom: 1px solid #f0f0f0;
@@ -98,7 +97,7 @@ const onCopy = () => {
   position: relative;
 
   .code-output {
-    min-height: 360px;
+    min-height: 340px;
     max-height: 500px;
     margin: 0;
     padding: 12px;
